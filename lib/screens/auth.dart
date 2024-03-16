41fb86f293744fd5e72fe19825501ffb93771520
Jason Park <jason808@naver.com>
2024-03-16 13:00:57 +0900

    Before AuthScreen

---
+++ lib/screens/auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
