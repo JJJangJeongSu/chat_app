2cae27b53bf99d44b63107f875ec9519a29f0836
Jason Park <jason808@naver.com>
2024-03-17 17:22:59 +0900

    log out

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,11 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("HEllo"),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.exit_to_app))
+        ],
+      ),
+      body: const Center(
+        child: Text("Your are logged in"),
       ),
     );
   }
